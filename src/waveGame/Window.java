5c2d459e832d03ba78e0a1145f8f95eade61fe8c
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-02 19:42:48 +0530

    window class setup
    location setup and starting set of game window

--- src/waveGame/Window.java
+++ src/waveGame/Window.java
@@ -9,12 +9,19 @@ public class Window extends Canvas {
 
 	private static final long serialVersionUID = 1L;
 	
-	public Window(int width, int height, String title) {
+	public Window(int width, int height, String title,gameMain game) {
 		JFrame frame=new JFrame(title);
 		
 		frame.setPreferredSize(new Dimension(width,height));
 		frame.setMaximumSize(new Dimension(width,height));
 		frame.setMinimumSize(new Dimension(width,height));
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.add(game);
+		frame.setVisible(true);
+		game.start();
 		
 	}
 }
