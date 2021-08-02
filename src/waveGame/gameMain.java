7b516fbd2ed767bf497d7c050091b973641a6a04
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-02 19:46:07 +0530

    Update gameMain.java
    main class setup and based function

--- src/waveGame/gameMain.java
+++ src/waveGame/gameMain.java
@@ -1,6 +1,17 @@
 package waveGame;
 
-public class gameMain {
+import java.awt.Canvas;
+
+public class gameMain extends Canvas implements Runnable {
+	
+	private static final long serialVersionUID = 7006633468047331596L;
+
+	public synchronized void start() {
+		
+	}
+	public void run() {
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
