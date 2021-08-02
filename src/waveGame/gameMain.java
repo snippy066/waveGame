3e3f624372e215814d3adecc3b9a71e84e8323d0
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-02 20:14:02 +0530

    Update gameMain.java

--- src/waveGame/gameMain.java
+++ src/waveGame/gameMain.java
@@ -6,19 +6,24 @@ public class gameMain extends Canvas implements Runnable {
 	
 	private static final long serialVersionUID = 7006633468047331596L;
 	
-	public static final int w=800;
-	public static final int d=w/12*9;
-	public static final String tit="Wave";
+	public static final int w=800;     //width
+	public static final int d=w/12*9;	//height
+	public static final String tit="Wave";	//title 
+	
+	public gameMain() {
+		new Window(w,d,tit,this);
+		System.out.println("game started");
+	}
 
 	public synchronized void start() {
-		new Window(w,d,tit,this);
+		
 	}
 	public void run() {
 		
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new gameMain();
 
 	}
 
