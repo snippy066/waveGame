a3df51c0ea7bb7b9e05dc06a8ac4d6974a5f3669
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-02 19:37:51 +0530

    window class updated
    added constructor and window size

--- src/waveGame/Window.java
+++ src/waveGame/Window.java
@@ -1,13 +1,20 @@
 package waveGame;
 
 import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class Window extends Canvas {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
-
+	public Window(int width, int height, String title) {
+		JFrame frame=new JFrame(title);
+		
+		frame.setPreferredSize(new Dimension(width,height));
+		frame.setMaximumSize(new Dimension(width,height));
+		frame.setMinimumSize(new Dimension(width,height));
+		
+	}
 }
