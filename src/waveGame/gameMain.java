fdbb09403380fd512fcf105566f72e949abc5098
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-02 19:52:00 +0530

    Update gameMain.java
    width height and title setup

--- src/waveGame/gameMain.java
+++ src/waveGame/gameMain.java
@@ -5,9 +5,13 @@ import java.awt.Canvas;
 public class gameMain extends Canvas implements Runnable {
 	
 	private static final long serialVersionUID = 7006633468047331596L;
+	
+	public static final int w=800;
+	public static final int d=w/12*9;
+	public static final String tit="Wave";
 
 	public synchronized void start() {
-		
+		new Window(w,d,tit,this);
 	}
 	public void run() {
 		
