184ae08d9d149557f34e3df8184fbdba577374bf
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-03 19:10:15 +0530

    added game loop

--- src/waveGame/gameMain.java
+++ src/waveGame/gameMain.java
@@ -37,10 +37,48 @@ public class gameMain extends Canvas implements Runnable {
 		}
 	}
 	
-	public void run() {
-		
-	}
-
+	 public void run() {
+	        long lastTime = System.nanoTime();
+	        double nsPerTick = 1000000000D / 64;
+	        
+	        int ticks = 0;
+	        int frames = 0;
+	        
+	        long lastTimer = System.currentTimeMillis();
+	        double delta = 0;
+	        
+	        while (running) {
+	            long now = System.nanoTime();
+	            delta += (now - lastTime) / nsPerTick;
+	            lastTime = now;
+	            boolean shouldRender = true;
+	            
+	            while (delta >= 1) {
+	                ticks++;
+	                tick();
+	                delta -= 1;
+	                shouldRender = true;
+	            }
+	            
+	            try {
+	                Thread.sleep(2);
+	            } catch (InterruptedException ex) {
+	                ex.printStackTrace();
+	            }
+	            
+	            if (shouldRender) {
+	                frames++;
+	                render();
+	            }
+	            
+	            if (System.currentTimeMillis() - lastTimer >= 1000) {
+	                lastTimer += 1000;
+	                System.out.println(ticks + " ticks, " + frames + " frames");
+	                frames = 0;
+	                ticks = 0;
+	            }
+	        }
+	 }
 	public static void main(String[] args) {
 		new gameMain();
 
