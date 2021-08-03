a22bd945f8036a990fe8eda80f7b06ac34b02121
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-03 19:05:08 +0530

    add threads to start and stop game

--- src/waveGame/gameMain.java
+++ src/waveGame/gameMain.java
@@ -10,14 +10,33 @@ public class gameMain extends Canvas implements Runnable {
 	public static final int d=w/12*9;	//height
 	public static final String tit="Wave";	//title 
 	
+	private Thread thread;
+	private boolean running=false;
+	
 	public gameMain() {
 		new Window(w,d,tit,this);
 		System.out.println("game started");
 	}
 
 	public synchronized void start() {
+		thread=new Thread();
+		thread.start();
+		running=true;
 		
 	}
+	
+	public synchronized void stop() {
+		try {
+			
+			thread.join();
+			running=false;
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public void run() {
 		
 	}
