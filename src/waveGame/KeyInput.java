dbdfea4a005f8af723631b94e810159c59f866b9
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-09-29 12:26:48 +0530

    Update KeyInput.java

--- src/waveGame/KeyInput.java
+++ src/waveGame/KeyInput.java
@@ -29,10 +29,24 @@ public class KeyInput extends KeyAdapter {
 					temp.setSpeedX(5);
 				
 			}
+			if(temp.getId()==ID.Player) {
+				//key events for player
+				if(key==KeyEvent.VK_UP)
+					temp.setSpeedY(-5);
+				if(key==KeyEvent.VK_LEFT) 
+					temp.setSpeedX(-5);
+				if(key==KeyEvent.VK_DOWN) 
+					temp.setSpeedY(5);
+				if(key==KeyEvent.VK_RIGHT) 
+					temp.setSpeedX(5);
+				
+			}
 			
 			//for player 2
 			
 		}
+		
+		if(key==KeyEvent.VK_ESCAPE) System.exit(1);
 		
 	}
 	public void keyReleased(KeyEvent e) {
@@ -50,6 +64,18 @@ public class KeyInput extends KeyAdapter {
 				if(key==KeyEvent.VK_S) 
 					temp.setSpeedY(0);
 				if(key==KeyEvent.VK_D) 
+					temp.setSpeedX(0);
+				
+			}
+			if(temp.getId()==ID.Player) {
+				//key events for player
+				if(key==KeyEvent.VK_UP)
+					temp.setSpeedY(0);
+				if(key==KeyEvent.VK_LEFT) 
+					temp.setSpeedX(0);
+				if(key==KeyEvent.VK_DOWN) 
+					temp.setSpeedY(0);
+				if(key==KeyEvent.VK_RIGHT) 
 					temp.setSpeedX(0);
 				
 			}
