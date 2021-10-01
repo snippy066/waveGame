1425b1b59fbc0c4ce276aebf1709d07fce06e1ed
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-10-02 00:24:23 +0530

    added trail for the basic enemy

--- src/waveGame/BasicTrail.java
+++ src/waveGame/BasicTrail.java
@@ -9,20 +9,31 @@ import java.awt.Rectangle;
 public class BasicTrail extends GameObject {
 	
 	private float alpha=1;
+	private float life;
+	
 	private Handler handler; 
 	private Color color;
+	private int width, height;
 
-	public BasicTrail(int x, int y,Color color, ID id) {
+	public BasicTrail(int x, int y,Color color,int width,int height,float life, ID id,Handler handler) {
 		super(x, y, id);
 		
+		this.handler=handler;
 		this.color=color;
-		
+		this.width=width;
+		this.height=height;
+		this.life=life;
 		
 	}
 
 	@Override
 	public void tick() {
-		
+		if(alpha>life) {
+			alpha-=life- 0.001f;
+			
+		}
+		else
+			handler.removeObject(this);
 		
 	}
 
@@ -30,7 +41,12 @@ public class BasicTrail extends GameObject {
 	public void render(Graphics g) {
 		Graphics2D g2d=(Graphics2D) g;
 		g2d.setComposite(makeTrasparent(alpha));
-	
+		
+	    g.setColor(color);
+	    g.fillRect(x, y, width, height);
+	    
+	    g2d.setComposite(makeTrasparent(1));
+		
 		
 	}
 	
